547cf07e8e177d0ce4d3fefe7a9fd2b90cdc086a
Nilesh Sonar <sonar.nilesh021992@gmail.com>
2025-10-10 00:12:27 +0530

    Commit 09/10/2025

---
+++ PythonLearn/Class02_09102025/Class02_PrintFun.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+#Multiple arguments
+print("Hello","Amol","Tejal",1997,1.5)
+
+#Separator
+print("Hello","Amol","Tejal",1997,1.5,sep="$")
+
+#New Line or Same line with End
+print("Hello","Amol","Tejal",1997,1.5,True,sep="*",end="_&&_")
+print("Hello","Amol","Tejal",1997,1.5,False,sep="*",end="\n\n")
+
+print("Hello","Amol","Tejal","Rahul",1997,1.5,sep="*",end="**")
+
---
+++ PythonLearn/Class02_09102025/Class02_Variable.py
@@ -0,0 +1,19 @@
+A=10    #Integer Data type
+B=3.15  #Float Data type
+print(A+B, end="\n")
+
+A="Amol" #Character Data type
+B="Tejal" #Character Data type
+print(A,B,end="\n")
+
+
+A=["Amol","Tejal"]  #List(Array) Data type
+print(A[0],A[1], end="\n")
+print(A[1],end="\n")
+
+A= True
+B= False
+print(A,end="\n")
+
+_Amol= 7277
+print(_Amol)
