5c91462fdd96d49f13bc4c8cbd70622bef4b1449
Nilesh Sonar <sonar.nilesh021992@gmail.com>
2025-10-12 00:47:26 +0530

    Commit 11/10/2025

--- PythonLearn/Class03_11102025/Ex09_UserInput.py
+++ PythonLearn/Class03_11102025/Ex09_UserInput.py
@@ -2,7 +2,7 @@ A = float(input("num1 = "))
 B = float(input("num2 = "))
 
 C = A + B
-print(C)
+print("Total =", C)
 
 print("Total Sum = ", float(A) + float(B))
 print("Total Mul = ", float(A) * float(B))
@@ -13,3 +13,4 @@ print("Total Sum = ", A + B)
 print("Total Mul = ", A * B)
 print("Total Div = ", A / B)
 print("Total Div = ", A - B)
+
